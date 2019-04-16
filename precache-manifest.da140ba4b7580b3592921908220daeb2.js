8b0c0e3eb6f12525f9b9b676432359a151365e32
rhzone <atgdut@gmail.com>
2019-04-17 01:03:53 +0800

    Updates

---
+++ precache-manifest.da140ba4b7580b3592921908220daeb2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e546b3e323215319a407",
+    "url": "/react-calculator/static/js/runtime~main.00b8a6fe.js"
+  },
+  {
+    "revision": "ea3c98c2bc72c60eaab5",
+    "url": "/react-calculator/static/js/main.7f9f8be6.chunk.js"
+  },
+  {
+    "revision": "baf961a1263ccd6a2d24",
+    "url": "/react-calculator/static/js/2.0f58faaa.chunk.js"
+  },
+  {
+    "revision": "ea3c98c2bc72c60eaab5",
+    "url": "/react-calculator/static/css/main.fb06f182.chunk.css"
+  },
+  {
+    "revision": "b1a4bf57aac78e233d3be0744378e775",
+    "url": "/react-calculator/index.html"
+  }
+];
